214b7719b645757e66fd6d80108200dba710985e
Gaëtan DUHAMEL <gaetan_duhamel@natural-solutions.eu>
2023-03-01 15:24:55 +0100

    feat: add flowbite UI

---
+++ src/lib/Display/Alert.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+	export let message = "Default message...."
+</script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Info alert!</span> {message}
+	</Alert>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
 	theme: {
 		extend: {
 			keyframes: {
@@ -20,5 +23,10 @@ module.exports = {
 			}
 		}
 	},
-	plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/typography')]
+	plugins: [
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/typography'),
+		require('flowbite/plugin')
+	],
+	darkMode: 'class'
 };
