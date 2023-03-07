e5e037ecb48dcfb5f0cfac37db9c2cc8d0945519
Gaëtan DUHAMEL <gaetan_duhamel@natural-solutions.eu>
2023-03-07 18:27:07 +0100

    fix: tmp disable SSR

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,34 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex';
-import { resolve } from 'path';
-import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-vercel'
+import preprocess from 'svelte-preprocess'
+import { mdsvex } from 'mdsvex'
+import { resolve } from 'path'
+import mdsvexConfig from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [
-		mdsvex(mdsvexConfig),
-		preprocess({
-			postcss: true
-		})
-	],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
+  preprocess: [
+    mdsvex(mdsvexConfig),
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		alias: {
-			$lib: resolve('./src/lib'),
-			$data: resolve('./src/data'),
-			$stores: resolve('./src/stores')
-		},
-		adapter: adapter()
-	}
-};
+  kit: {
+    alias: {
+      $lib: resolve('./src/lib'),
+      $data: resolve('./src/data'),
+      $stores: resolve('./src/stores'),
+    },
+    adapter: adapter(),
+    ssr: false,
+    prerender: {
+      enabled: false,
+    },
+  },
+}
 
-export default config;
+export default config
